9ed9bb0b8f9910faec94abf568f14ec8a3560ea6
AngelGigov <angel_gigov@yahoo.com>
2023-09-24 11:19:02 +0300

    Task 7 have been completed

--- Python-Fundamentals/22.09 - Exercise Data Types and Variables/07_water_overflow.py
+++ Python-Fundamentals/22.09 - Exercise Data Types and Variables/07_water_overflow.py
@@ -1,1 +1,16 @@
-#
+#Read User Input
+number_of_iterations = int(input())
+tank_capacity = 255
+liters_poured =  0
+
+#Logic
+for i in range(number_of_iterations):
+    liters_to_pour = int(input())
+    if liters_to_pour > (tank_capacity - liters_poured):
+        print('Insufficient capacity!')
+        continue
+    else:
+        liters_poured += liters_to_pour
+
+#Print Output
+print(liters_poured)
