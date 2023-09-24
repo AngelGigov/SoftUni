8be2981ecbeb9f39af41d7982a0bd4e66cdb3b34
AngelGigov <angel_gigov@yahoo.com>
2023-09-24 14:39:40 +0300

    Task 8 have been completed

--- Python-Fundamentals/22.09 - Exercise Data Types and Variables/08_party_profit.py
+++ Python-Fundamentals/22.09 - Exercise Data Types and Variables/08_party_profit.py
@@ -1,11 +1,23 @@
 #User Input
 companions_count = int(input())
 days = int(input())
+coins = 0
 
 #Logic
-
-
-
+for day in range(1, days + 1):
+    if day % 10 == 0:
+        companions_count -= 2
+    if day % 15 == 0:
+        companions_count += 5
+    if day % 3 == 0:
+        coins -= companions_count * 3
+    if day % 5 == 0:
+        coins += companions_count * 20
+        if day % 3 == 0:
+            coins -= companions_count * 2
+    coins += 50
+    coins -= companions_count * 2
 
 #Print Output
-print(f'{companions_count} companions received {coins} coins each.')
+price = coins // companions_count
+print(f'{companions_count} companions received {price} coins each.')
