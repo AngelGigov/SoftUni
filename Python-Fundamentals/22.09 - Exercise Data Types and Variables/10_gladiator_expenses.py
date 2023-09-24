5ee7445262107a74e708fec96d38ad1812cbed9e
AngelGigov <angel_gigov@yahoo.com>
2023-09-24 15:06:06 +0300

    Task 10 have been Initiated

---
+++ Python-Fundamentals/22.09 - Exercise Data Types and Variables/10_gladiator_expenses.py
@@ -0,0 +1,14 @@
+#User Input
+lost_fights_count = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+expenses = 0
+
+#Logic
+
+
+
+#Print Output
+print(f"Gladiator expenses: {expenses:.2f} aureus")
