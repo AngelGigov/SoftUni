cb7abd914224341905e78f218603cdce4d0e3fb9
AngelGigov <angel_gigov@yahoo.com>
2023-09-24 15:18:23 +0300

    Task 10 for excercise Data types have been solved

--- Python-Fundamentals/22.09 - Exercise Data Types and Variables/10_gladiator_expenses.py
+++ Python-Fundamentals/22.09 - Exercise Data Types and Variables/10_gladiator_expenses.py
@@ -7,8 +7,15 @@ armor_price = float(input())
 expenses = 0
 
 #Logic
+total_helmet_lost = lost_fights_count // 2
+total_sword_lost = lost_fights_count // 3
+total_shield_lost = lost_fights_count // (3 * 2)
+total_armor_lost = total_shield_lost // 2
 
-
+expenses = total_helmet_lost * helmet_price \
+    + total_armor_lost * armor_price \
+    + total_sword_lost * sword_price \
+    + total_shield_lost * shield_price
 
 #Print Output
 print(f"Gladiator expenses: {expenses:.2f} aureus")
