edc0ffbabf94eb892bd3bb6cce8c2853dae46393
AngelGigov <angel_gigov@yahoo.com>
2023-09-24 15:47:04 +0300

    Additional Excersises Have Been Initiated.

---
+++ Python-Fundamentals/22.09 - Exercise Data Types and Variables/Additional_excersise/01_exchange_integers.py
@@ -0,0 +1,13 @@
+#Read User Input
+num_a = int(input())
+num_b = int(input())
+
+#Print output
+print(f'''
+Before:
+a = {num_a}
+b = {num_b}
+After:
+a = {num_b}
+b = {num_a}''')
+
---
+++ Python-Fundamentals/22.09 - Exercise Data Types and Variables/Additional_excersise/02_prime_number_checker.py
@@ -0,0 +1,14 @@
+#User Input
+number = int(input())
+
+is_prime = False
+
+if number > 1:
+    # check for factors
+    for i in range(2, number):
+        if (number % i) == 0:
+            is_prime = True
+            break
+
+    # check if flag is True
+print(is_prime)
